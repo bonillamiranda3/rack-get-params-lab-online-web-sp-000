8ad07ac6bd308f6d29acd03c99fddc0efb6c6ed6
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-15 23:38:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,10 +17,9 @@ class Application
     elsif req.path.match(/cart/)
   @@cart.empty? ? resp.write("Your cart is empty") :@@cart.each {|item| resp.write("#{item}\n")}
     elsif req.path.match(/add/)
-    search_term = req.params["item"]
-    resp.write add_items_to_cart(search_term)
-
-
+      search_term = req.params["item"]
+      resp.write add_items_to_cart(search_term)
+    
     else
       resp.write "Path Not Found"
     end
